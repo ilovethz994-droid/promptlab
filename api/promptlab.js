615cb10b0d3bf3e246bae0e55ae50ac53c5fb546
ilovethz994-droid <ilovethz994@gmail.com>
2026-09-18 00:36:24 +0800

    fix: make PromptLab API CommonJS-safe

--- api/promptlab.js
+++ api/promptlab.js
@@ -1,4 +1,4 @@
-import { createHash, randomUUID } from 'node:crypto'
+const { createHash, randomUUID } = require('node:crypto')
 const json = (res, status, body) => res.status(status).json(body)
 
 function accessHash(code) {
@@ -117,7 +117,7 @@ function normalizeResult(data, originalPrompt) {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
